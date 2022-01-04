7f9a30200bf024e231b91e7af3e51470f0faa8fc
hsariaslan <hakan_dnz_000@hotmail.com>
2022-01-04 18:52:58 +0300

    Update auth routes middleware to fix 419 error

--- server/routes/api.php
+++ server/routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['api']], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/register', [AuthController::class, 'register']);
