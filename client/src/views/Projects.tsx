b377bd0e9eea71fdfccfd1692380e5cfd2da1a83
hakan <hakan_dnz_000@hotmail.com>
2022-02-06 22:22:22 +0300

    Update datatable in projects view

--- client/src/views/Projects.tsx
+++ client/src/views/Projects.tsx
@@ -2,16 +2,14 @@ import React, {useEffect} from "react";
 import {useDispatch} from 'react-redux';
 import {useNavigate} from "react-router-dom";
 import {update} from '../features/title/title';
-import {DataGrid, GridApi, GridCellValue, GridColDef} from '@mui/x-data-grid';
 import axios from "axios";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import DoneIcon from "@mui/icons-material/Done";
-import DoneAllIcon from "@mui/icons-material/DoneAll";
-import DoDisturbOnIcon from "@mui/icons-material/DoDisturbOn";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -20,7 +18,7 @@ import Button from '@mui/material/Button';
 const Projects = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [rows, setRows] = React.useState([]);
+    const [rows, setRows] = React.useState<any>([]);
     const [id, setId] = React.useState(0);
     const [openDialog, setOpenDialog] = React.useState(false);
 
@@ -46,143 +44,52 @@ const Projects = () => {
         return navigate('/cancel-select/' + id);
     };
 
-    const columns: GridColDef[] = [
-        {field: 'id', headerName: 'ID', width: 70},
-        {field: 'title', headerName: 'Proje Adı', width: 400},
-        {
-            field: 'status',
-            headerName: 'Durumu',
-            width: 200,
-            renderCell: (params) => {
-                const findValue = () => {
-                    const api: GridApi = params.api;
-                    const thisRow: Record<string, GridCellValue> = {};
-
-                    api
-                        .getAllColumns()
-                        .filter((c: any) => c.field !== "__check__" && !!c)
-                        .forEach(
-                            (c: any) => (thisRow[c.field] = params.getValue(params.id, c.field))
-                        );
-                    return thisRow.status;
-                }
-                let returnElement: any;
-
-                switch (findValue()) {
-                    case 0:
-                        returnElement = (<div className="flex items-center gap-x-1">
-                            <AccessTimeIcon className="text-gray-500"/>
-                            <span className="text-gray-500 mt-1"><i>Bekliyor</i></span>
-                        </div>);
-                        break;
-                    case 1:
-                        returnElement = (<div className="flex items-center gap-x-1">
-                            <CheckCircleOutlineIcon className="text-blue-500"/>
-                            <span className="text-blue-500 mt-1"><i>Kabul Edildi</i></span>
-                        </div>);
-                        break;
-                    case 2:
-                        returnElement = (<div className="flex items-center gap-x-1">
-                            <DoneIcon className="text-cyan-500"/>
-                            <span className="text-cyan-500 mt-1"><i>Teslim Edildi</i></span>
-                        </div>);
-                        break;
-                    case 3:
-                        returnElement = (<div className="flex items-center gap-x-1">
-                            <DoneAllIcon className="text-green-500"/>
-                            <span className="text-green-500 mt-1"><i>Not Verildi</i></span>
-                        </div>);
-                        break;
-                }
-
-                return (
-                    <div>
-                        {returnElement}
-                    </div>
-                );
-            }
-        },
-        {field: 'deadline', headerName: 'Bitiş Tarihi', width: 200},
-        {
-            field: "actions",
-            headerName: "İşlemler",
-            sortable: false,
-            width: 130,
-            renderCell: (params) => {
-                const findRow = () => {
-                    const api: GridApi = params.api;
-                    const thisRow: Record<string, GridCellValue> = {};
-
-                    api
-                        .getAllColumns()
-                        .filter((c: any) => c.field !== "__check__" && !!c)
-                        .forEach(
-                            (c: any) => (thisRow[c.field] = params.getValue(params.id, c.field))
-                        );
-                    return [thisRow.id as number, thisRow.actions as number];
-                };
-                const action = () => {
-                    let foundAction: any = findRow();
-                    return foundAction[1] as number;
-                };
-                const selection = (e: any) => {
-                    e.stopPropagation(); // don't select this row after clicking
-                    let foundId: any = findRow();
-                    return navigate('/select-project/' + foundId[0]);
-                };
-                const cancel = (e: any) => {
-                    e.stopPropagation(); // don't select this row after clicking
-                    let foundId: any = findRow();
-                    setId(foundId);
-                    handleClickOpen();
-                };
-
-                return (
-                    <List>
-                        {action() === 0 ?
-                            <ListItem title="Tercih Yap">
-                                <AddCircleIcon onClick={selection}
-                                               className="text-blue-500 cursor-pointer hover:text-blue-800"/>
-                            </ListItem>
-                            : action() === 1 ?
-                                <ListItem title="Tercihi İptal Et">
-                                    <DoDisturbOnIcon onClick={cancel}
-                                                     className="text-red-500 cursor-pointer hover:text-red-800"/>
-                                </ListItem>
-                                : action() === 2 ?
-                                    <ListItem title="Daha fazla tercih yapılamaz">
-                                        <AddCircleIcon onClick={cancel}
-                                                       className="text-gray-500 cursor-not-allowed"/>
-                                    </ListItem>
-                                    : action() === 3 ?
-                                        <ListItem title="Bu proje başka bir öğrenci tarafından alındı">
-                                            <AddCircleIcon onClick={cancel}
-                                                           className="text-gray-500 cursor-not-allowed"/>
-                                        </ListItem>
-                                        : action() === 4 ?
-                                            <ListItem title="Tercih iptal edilemez">
-                                                <AddCircleIcon onClick={cancel}
-                                                               className="text-gray-500 cursor-not-allowed"/>
-                                            </ListItem>
-                                            : null
-                        }
-
-                    </List>
-                );
-            }
-        },
-    ];
-
     return (
         <div style={{height: 400, width: '100%'}}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={25}
-                rowsPerPageOptions={[25]}
-                disableSelectionOnClick
-                disableColumnMenu
-            />
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} size="small" aria-label="a dense table">
+                    <TableHead className="bg-blue-500">
+                        <TableRow>
+                            <TableCell>
+                                <span className="text-white">Proje Adı</span>
+                            </TableCell>
+                            <TableCell>
+                                <span className="text-white">Öğretim Üyesi</span>
+                            </TableCell>
+                            <TableCell align="center">
+                                <span className="text-white">Tercih Edenlerin Sayısı</span>
+                            </TableCell>
+                            <TableCell align="center">
+                                <span className="text-white">Durumu</span>
+                            </TableCell>
+                            <TableCell align="center">
+                                <span className="text-white">Son Teslim Tarihi</span>
+                            </TableCell>
+                            <TableCell align="right">
+                                <span className="text-white">İşlemler</span>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row:any) => (
+                            <TableRow
+                                key={row.title}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.title}
+                                </TableCell>
+                                <TableCell>{row.user?.name + ' ' + row.user?.surname}</TableCell>
+                                <TableCell align="center">{row.selection_count}</TableCell>
+                                <TableCell align="center">{row.status}</TableCell>
+                                <TableCell align="center">{row.deadline}</TableCell>
+                                <TableCell align="right">{row.actions}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
             <Dialog
                 open={openDialog}
                 onClose={handleClose}
@@ -201,6 +108,6 @@ const Projects = () => {
             </Dialog>
         </div>
     );
-};
+}
 
 export default Projects;
